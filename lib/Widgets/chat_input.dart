aa4fda8671b81eab39c1485f80378b4f2f47736c
Sanket80 <sanketkadamsss4@gmail.com>
2024-01-23 16:56:18 +0530

    Done with Input Form Field

--- lib/Widgets/chat_input.dart
+++ lib/Widgets/chat_input.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
 class ChatInput extends StatelessWidget {
-  const ChatInput({super.key});
+  ChatInput({super.key});
+
+  final chatMessageController = TextEditingController();
+
+  void onSendButtonPressed() {
+    print(chatMessageController.text);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,8 +26,28 @@ class ChatInput extends StatelessWidget {
             onPressed: () {},
             icon: const Icon(Icons.add, color: Colors.white),
           ),
+          Expanded(
+            child: TextField(
+              keyboardType: TextInputType.multiline,
+              maxLines: 5,
+              minLines: 1,
+              textCapitalization: TextCapitalization.sentences,
+              controller: chatMessageController,
+              style: TextStyle(
+                color: Colors.white,
+              ),
+              decoration: InputDecoration(
+                hintText: 'Type a message',
+                hintStyle: TextStyle(
+                  color: Colors.grey[600],
+                ),
+                border: InputBorder.none,
+                contentPadding: const EdgeInsets.all(20),
+              ),
+            ),
+          ),
           IconButton(
-            onPressed: () {},
+            onPressed: onSendButtonPressed,
             icon: const Icon(Icons.send, color: Colors.white),
           ),
         ],
--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,34 +1,125 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  LoginPage({super.key});
+
+  final _formKey = GlobalKey<FormState>();
+
+  void loginUser() {
+    if (_formKey.currentContext!=null && _formKey.currentState!.validate()) {
+      print('Username: ${UserNameController.text}');
+      print('Password: ${PasswordController.text}');
+      print('Login User');
+    } else {
+      print('Not Successful');
+    }
+  }
+
+  final UserNameController = TextEditingController();
+  final PasswordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Let\'s Sign you in',
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Text(
+                'Let\'s Sign you in',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            const SizedBox(height: 14),
-            const Text(
-              'Welcome back! \n You\'ve been missed!',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.w500,
+              const SizedBox(height: 14),
+              const Text(
+                'Welcome back! \n You\'ve been missed!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                ),
               ),
-            ),
-            const SizedBox(height: 14),
-            Image.network('https://img.freepik.com/premium-vector/chat-app-logo-sms-messenger-label-design-mobile-app-online-conversation-with-texting-message-ui-design-concept-vector-illustration_172533-1513.jpg', height: 200),
-          ],
+              const SizedBox(height: 14),
+              Image.network(
+                  'https://img.freepik.com/premium-vector/chat-app-logo-sms-messenger-label-design-mobile-app-online-conversation-with-texting-message-ui-design-concept-vector-illustration_172533-1513.jpg',
+                  height: 200),
+              const SizedBox(height: 14),
+
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your username';
+                        }else if(value.length < 6){
+                          return 'Username must be atleast 6 characters long';
+                        }
+                        return null; // if the value is valid
+                      },
+                      controller: UserNameController,
+                      decoration: InputDecoration(
+                        hintText: 'Username',
+                        hintStyle: TextStyle(
+                          color: Colors.grey[500],
+                        ),
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(height: 12),
+
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your password';
+                        }else if(value.length < 6){
+                          return 'Password must be atleast 6 characters long';
+                        }
+                        return null; // if the value is valid
+                      },
+                      controller: PasswordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: 'Password',
+                        hintStyle: TextStyle(
+                          color: Colors.grey[500],
+                        ),
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 14),
+              ElevatedButton(
+                onPressed: loginUser,
+                style: ButtonStyle(
+                  padding: MaterialStateProperty.all<EdgeInsets>(
+                    const EdgeInsets.all(8),
+                  ),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(1),
+                    ),
+                  ),
+                ),
+                child: Text(
+                  'Login',
+                  style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
