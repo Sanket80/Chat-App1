18ef491b0492762f2caf669818c08d0e300bbbe6
Sanket80 <sanketkadamsss4@gmail.com>
2024-01-24 16:24:24 +0530

    Explored Various Packages

---
+++ lib/Utils/textfield_styles.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ThemeTextStyle{
+  static TextStyle loginTextFieldStyle = GoogleFonts.poppins(
+      textStyle: TextStyle(
+        color: Colors.grey[500],
+      ),
+  );
+}
--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,5 +1,8 @@
+import 'package:chat_app/Utils/textfield_styles.dart';
 import 'package:chat_app/chat_page.dart';
 import 'package:flutter/material.dart';
+import 'package:social_media_buttons/social_media_button.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class LoginPage extends StatelessWidget {
   LoginPage({super.key});
@@ -22,6 +25,7 @@ class LoginPage extends StatelessWidget {
 
   final UserNameController = TextEditingController();
   final PasswordController = TextEditingController();
+  final _mainUrl = 'https://www.linkedin.com/in/sanket-kadam-0905a6225/';
 
   @override
   Widget build(BuildContext context) {
@@ -29,101 +33,142 @@ class LoginPage extends StatelessWidget {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(24.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const Text(
-                'Let\'s Sign you in',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Image.asset(
+                  'assets/banner_image.png',
+                  height: 200,),
+                const SizedBox(height: 24),
+                const Text(
+                  'Let\'s Sign you in',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 14),
-              const Text(
-                'Welcome back! \n You\'ve been missed!',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w500,
+                const SizedBox(height: 14),
+                const Text(
+                  'Welcome back! \n You\'ve been missed!',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 14),
-              Image.network(
-                  'https://img.freepik.com/premium-vector/chat-app-logo-sms-messenger-label-design-mobile-app-online-conversation-with-texting-message-ui-design-concept-vector-illustration_172533-1513.jpg',
-                  height: 200),
-              const SizedBox(height: 14),
-              Form(
-                key: _formKey,
-                child: Column(
-                  children: [
-                    TextFormField(
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter your username';
-                        } else if (value.length < 6) {
-                          return 'Username must be atleast 6 characters long';
-                        }
-                        return null; // if the value is valid
-                      },
-                      controller: UserNameController,
-                      decoration: InputDecoration(
-                        hintText: 'Username',
-                        hintStyle: TextStyle(
-                          color: Colors.grey[500],
+                const SizedBox(height: 24),
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your username';
+                          } else if (value.length < 6) {
+                            return 'Username must be atleast 6 characters long';
+                          }
+                          return null; // if the value is valid
+                        },
+                        controller: UserNameController,
+                        decoration: InputDecoration(
+                          hintText: 'Username',
+                          hintStyle: ThemeTextStyle.loginTextFieldStyle,
+                          border: OutlineInputBorder(),
                         ),
-                        border: OutlineInputBorder(),
+                      ),
+                      const SizedBox(height: 12),
+                      TextFormField(
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your password';
+                          } else if (value.length < 6) {
+                            return 'Password must be atleast 6 characters long';
+                          }
+                          return null; // if the value is valid
+                        },
+                        controller: PasswordController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: 'Password',
+                          hintStyle: ThemeTextStyle.loginTextFieldStyle,
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 14),
+                ElevatedButton(
+                  onPressed: () {
+                    loginUser(context);
+                  },
+                  style: ButtonStyle(
+                    padding: MaterialStateProperty.all<EdgeInsets>(
+                      const EdgeInsets.all(8),
+                    ),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(1),
                       ),
                     ),
-                    const SizedBox(height: 12),
-                    TextFormField(
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter your password';
-                        } else if (value.length < 6) {
-                          return 'Password must be atleast 6 characters long';
-                        }
-                        return null; // if the value is valid
-                      },
-                      controller: PasswordController,
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        hintText: 'Password',
-                        hintStyle: TextStyle(
-                          color: Colors.grey[500],
+                  ),
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 14),
+                GestureDetector(
+                  onTap: () async {
+                    if(!await launch(_mainUrl)){
+                      throw 'Could not launch $_mainUrl';
+                    }
+                  },
+                  child: Column(
+                    children: [
+                      Text('Connect with me on LinkedIn',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.deepPurpleAccent,
                         ),
-                        border: OutlineInputBorder(),
                       ),
+                      const SizedBox(height: 6),
+                      Text('$_mainUrl',
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.deepPurpleAccent,
+                        ),
+                      ),
+                    ]
+                  )
+                ),
+                const SizedBox(height: 24),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    SocialMediaButton.linkedin(
+                      url: 'https://www.linkedin.com/in/sanket-kadam-0905a6225/',
+                      size: 50,
+                      color: Colors.blue,
+                    ),
+                    SocialMediaButton.github(
+                      url: 'https://github.com/Sanket80',
+                      size: 50,
+                      color: Colors.black,
                     ),
                   ],
-                ),
-              ),
-              const SizedBox(height: 14),
-              ElevatedButton(
-                onPressed: () {
-                  loginUser(context);
-                },
-                style: ButtonStyle(
-                  padding: MaterialStateProperty.all<EdgeInsets>(
-                    const EdgeInsets.all(8),
-                  ),
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(1),
-                    ),
-                  ),
-                ),
-                child: Text(
-                  'Login',
-                  style: TextStyle(
-                    fontSize: 28,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-              ),
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
